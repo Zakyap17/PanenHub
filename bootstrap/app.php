3a7b9de5f672ad851e4a1c2a32c0ef30642315ed
Zakyap17 <zakyaprilian17@gmail.com>
2026-05-28 14:53:32 +0700

    Fix target class role.mitra does not exist and ensure missing DB columns are added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role.mitra' => \App\Http\Middleware\RoleMitra::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
